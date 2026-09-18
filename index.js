a5ade9e011e071748e767fa7d971047dc54de507
Agam Saxena <agamsaxena80@gmail.com>
2026-09-18 06:54:23 +0530

    CSS RENDER

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 function sayHello() {
-            console.log("Hi there!");
+            alert("Hi there!");
         }
+function submitForm() {
+
+    const firstName = document.querySelector("#firstName").value;
+    const lastName = document.querySelector("#lastName").value;
+
+    console.log(firstName);
+    console.log(lastName);
+
+    if (firstName === "" || lastName === "") {
+        alert("Please enter your first and last name.");
+    } 
+    else {
+        alert("Form submitted!");
+    }
+}
